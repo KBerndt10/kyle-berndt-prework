aa9eb38e6da82586feb79fb4ff1467b6d1997797
Kyle Berndt <bernd165@umn.edu>
2020-05-06 15:03:59 -0500

    Made js basics activity 1 log results

--- JavaScript_Basics_Assessment/activity-1.js
+++ JavaScript_Basics_Assessment/activity-1.js
@@ -2,5 +2,12 @@ const product = 9 * 9;
 const quotient = 56 / 12;
 const remainder = 281 % 9;
 const inf = 18 / 0;
-const concat = '56' + '92'
+const concat = '56' + '92';
 const falseBool = 42 >= 52;
+
+console.log(`9 * 9 = ${product}`);
+console.log(`56 / 12 = ${quotient}`);
+console.log(`281 % 9 = ${remainder}`);
+console.log(`18 / 0 = ${inf}`);
+console.log(`\'56\' + \'92\' = ${concat}`);
+console.log(`42 >= 52 = ${falseBool}`);
