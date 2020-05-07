547ce99d3053a5ad3417dd18d3e5bcfadf19b331
Kyle Berndt <bernd165@umn.edu>
2020-05-07 15:02:33 -0500

    Added words and word randomization

--- Module-2_Assessment/assets/javascript/game.js
+++ Module-2_Assessment/assets/javascript/game.js
@@ -10,16 +10,62 @@ const game = {
     lettersGuessed: [],
     lettersKnown: [],
     wins: 0,
-    word: "Test",
+    winMessage: '',
+    word: '',
+    topic: {},
+    topics: [
+        {
+            word: "Aragorn",
+            pic: "aragorn.jpg",
+            msg: "Aragorn, heir of Isildur"
+        },
+        {
+            word: "Minas Tirith",
+            pic: "minastirith.jpg",
+            msg: "Minas Tirith, capitol of Gondor"
+        },
+        {
+            word: "Gollum",
+            pic: "gollum.jpg",
+            msg: "Gollum, Lover of the Precious"
+        },
+        {
+            word: "Legolas",
+            pic: "legolas.jpg",
+            msg: "Legolas Greenleaf"
+        },
+        {
+            word: "Gimli",
+            pic: "gimli.jpg",
+            msg: "Gimli, son of Glóin"
+        },
+    ],
+    usedTopics: [],
 
     // Resets the game for a new round
     newRound: function () {
         this.guesses = maxGuesses;
         this.lettersGuessed = [];
         this.lettersKnown = [];
-        this.word = 'Test';
-        this.wins = 0;
+        this.selectNextWord();
         this.updateDOM();
+    },
+
+    // selects the next topic at random, and moves it into the used topics list and sets it as active
+    // Will recycle the used topics if no new ones are left
+    selectNextWord: function () {
+        // Reset available topics if all have been used
+        if (this.topics.length <= 0) {
+            this.topics = this.usedTopics;
+            this.usedTopics = [];
+        }
+
+        const index = Math.floor(Math.random() * this.topics.length);
+        this.topic = this.topics[index];
+        this.topics[index] = this.topics[this.topics.length-1];
+        this.topics.pop();
+        this.usedTopics.push(this.topic);
+        this.word = this.topic.word;
     },
 
     // Handles key presses as part of the game
@@ -27,15 +73,14 @@ const game = {
     // determines whether the letter has been guessed or not and takes the appropriate response
     handleInput: function (event) {
         const key = event.key.toUpperCase();
-        console.log(key);
 
-        if(!game.lettersGuessed.includes(key) && !game.lettersKnown.includes(key) && game.isLetter(key)){
-            if(game.word.toUpperCase().includes(key)){
+        if (!game.lettersGuessed.includes(key) && !game.lettersKnown.includes(key) && game.isLetter(key)) {
+            if (game.word.toUpperCase().includes(key)) {
                 game.lettersKnown.push(key);
                 game.checkWin();
                 game.updateWord();
-            } 
-            else{
+            }
+            else {
                 game.lettersGuessed.push(key);
                 game.guessWrong();
                 game.updateLetters();
@@ -43,42 +88,45 @@ const game = {
         }
     },
 
-    checkWin: function(){
-        for(let i = 0; i < this.word.length; i++){
+    // Determines whether or not you have won
+    checkWin: function () {
+        for (let i = 0; i < this.word.length; i++) {
             const ltr = this.word.charAt(i).toUpperCase();
-            if(!this.lettersKnown.includes(ltr)){
+            if (!this.lettersKnown.includes(ltr) && this.isLetter(ltr)) {
                 return;
             }
         }
         this.wins++;
+        this.winMessage = this.topic.msg;
         this.newRound();
     },
 
-    guessWrong: function(){
+    // Determines whether or not you have lost
+    guessWrong: function () {
         this.guesses--;
-        if(this.guesses <= 0){
+        if (this.guesses <= 0) {
             this.newRound();
         }
     },
 
     // returns true if ch is a (single) letter A-Z or a-z
-    isLetter(ch){
+    isLetter(ch) {
         return ch.length === 1 && (ch.toUpperCase() != ch.toLowerCase());
     },
 
     // Updates the number of guesses and previously guessed letters elements on the page
-    updateLetters: function() {
+    updateLetters: function () {
         game.lettersSpan.innerText = this.lettersGuessed.join();
         game.guessesSpan.innerText = this.guesses;
     },
 
     // Creates and display the partial word based on known letters and displays it
-    updateWord: function(){
+    updateWord: function () {
         let disp = '';
         const wordUpper = this.word.toUpperCase();
-        for(let i = 0; i < this.word.length; i++){
+        for (let i = 0; i < this.word.length; i++) {
             const letter = wordUpper.charAt(i);
-            if(this.lettersKnown.includes(letter) || !this.isLetter(letter)){
+            if (this.lettersKnown.includes(letter) || !this.isLetter(letter)) {
                 disp += letter;
             } else {
                 disp += ' _ ';
@@ -92,6 +140,7 @@ const game = {
         this.updateLetters();
         this.updateWord();
         game.winCountH4.innerText = this.wins;
+        game.winMessageSpan.innerText = this.winMessage;
     },
 }
 
